Fix image analysis scaling

src/northern_lights_forecast/browser.py
@@ -49,13 +49,31 @@ def navigate(browser):
         dy: float
             The gradient returned by navigate()
     """
+    # # With the requests module
+    # headers = {
+    #     "User-Agent": "Mozilla/5.0 (Windows NT 5.1; rv:7.0.1) \
+    # Gecko/20100101 Firefox/7.0.1"
+    # }
+    # results = requests.get(
+    #     "https://flux.phys.uit.no/cgi-bin/mkascii.cgi?site=tro2a&year=2020&month" +
+    #     "=1&day=1&res=1min&pwd=&format=html&comps=DHZ&RTData=+Get+Realtime+Data+"
+    #     , headers=headers
+    # )
+    # # if check:
+    # print([results.ok])
+    # src = results.content
+    # print(src)
+    # soup = BeautifulSoup(src, "lxml")
+    # txt = soup.find("a", class_="css-1ej4hfo").text.upper()
+
+    # With the selenium module
     browser.get(
         "https://flux.phys.uit.no/cgi-bin/mkascii.cgi?site=tro2a&year=2020&"
         + "month=1&day=1&res=1min&pwd=&format=html&comps=DHZ&RTData=+Get+Realtime+Data+"
     )
     time.sleep(5)
-
     pure = browser.find_element_by_xpath("/html/body/pre")
+
     saved = pure.text.splitlines()
     for v, ss in enumerate(saved):
         saved[v] = ss.split()
@@ -73,3 +91,7 @@ def navigate(browser):
     dy = np.gradient(y)
 
     return np.min(dy)
+
+
+if __name__ == "__main__":
+    open_browser(hide=False)

src/northern_lights_forecast/image_analysis.py
@@ -5,6 +5,7 @@ https://stackoverflow.com/questions/60051941/find-the-
 coordinates-in-an-image-where-a-specified-colour-is-detected
 """
 import cv2
+import matplotlib.pyplot as plt
 import numpy as np
 from scipy.signal import savgol_filter
 
@@ -79,7 +80,7 @@ def main(scaling):
     dy = dy[int(len(x_) * 0.8) :]
 
     # === < Plot the result > ===
-    import matplotlib.pyplot as plt
+    # import matplotlib.pyplot as plt
 
     y_ = savgol_filter(y_i, 501, 3)  # window size 501, polynomial order 3
     # Rescale x-axis to 22-hour plot
@@ -90,6 +91,7 @@ def main(scaling):
     plt.imshow(im)
     plt.plot(x, y / scaling, "b")
     plt.plot(x_, -y_ / scaling, "r")
+
     # plt.figure()
     # plt.plot(x_[int(len(x_) * .8):], y_[int(len(x_) * .8):], 'r')
     # plt.savefig('after.png', dpi=200)

src/northern_lights_forecast/img.py
@@ -18,13 +18,16 @@ def download():
     Returns:
         cv2 image: the downloaded file as `.jpg`
     """
-    file = "Last24_tro2a.gif"
+    file = "assets/Last24_tro2a.gif"
     if os.path.exists(file):
         os.remove(file)
 
     url = "https://flux.phys.uit.no/Last24/Last24_tro2a.gif"
+    # saveas = "down.gif"
+    # wget.download(url, out="assets/saveas")
     wget.download(url, out="assets/Last24_tro2a.gif")
 
+    # gif = cv2.VideoCapture("assets/saveas")
     gif = cv2.VideoCapture("assets/Last24_tro2a.gif")
     ret, frame = gif.read()
     cv2.imwrite("assets/images.jpg", frame)
@@ -46,27 +49,31 @@ def read(image):
     images = np.hstack([image[360:530, 1:40, :]])
     data = pytesseract.image_to_boxes(images)
 
+    print("")
     d = data.split("\n")
     d = [data for data in d if len(data) > 1]
+    # print(d)
     d = d[::-1]
     # print(d)
     c = 0
+    y_level = 0
     lim_0 = ""
     y_0 = ""
     lim_1 = ""
     y_1 = ""
     for ell in d:
         ell = ell.split()
-        if ell[0] == "-":
+        if ell[0] != "-" and y_level != int(ell[2]):
+            y_level = int(ell[2])
             c += 1
-        else:
-            if c == 0:
+        elif y_level == int(ell[2]):
+            if c == 1:
                 lim_0 += ell[0]
                 y_0 = ell[2]
-            elif c == 1:
+            elif c == 2:
                 lim_1 += ell[0]
                 y_1 = ell[2]
-        if c >= 2:
+        if c >= 3:
             break
     lim_0 = float(lim_0[::-1])
     y_b = float(y_0)
@@ -107,7 +114,7 @@ def read(image):
 
     # print(f'Pixels to nT: {abs(y_t - y_b)} to {abs(lim_1 - lim_0)}')
     # print(f'Increase by {round(abs(lim_1 - lim_0) / abs(y_t - y_b), 2)} times')
-    return round(abs(lim_1 - lim_0) / abs(y_t - y_b), 2)
+    return 1 / round(abs(lim_1 - lim_0) / abs(y_t - y_b), 4)
 
 
 def find_colour(image):