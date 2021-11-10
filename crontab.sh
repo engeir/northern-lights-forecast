877b5f03ba043998561bc590b26894345272e699
Eirik Rolland Enger <eirroleng@gmail.com>
2021-11-10 20:55:23 +0100

    Check for tesseract and in crontab script

--- crontab.sh
+++ crontab.sh
@@ -11,15 +11,25 @@ done
 # here's the key part: remove the parsed options from the positional params
 shift $(( OPTIND - 1 ))
 
-# now, $1=="git", $2=="pull"
-
 # username=$(whoami)
 PWD=$(pwd)
 exe=$(python -c "import sys;print(sys.executable)")
-line="0 0-4,18-23 * 9-12,1-3 * export DISPLAY=:0 && cd $PWD && $exe src/northern_lights_forecast/__main__.py >> t.txt 2>&1"
+if ! command -v tesseract > /dev/null 2>&1
+then
+    newpath="# Tesseract was not found. Make sure it is installed and is in PATH."
+else
+    tesseract=$(dirname "$(which tesseract)")
+    newpath="PATH=\$PATH:$tesseract"
+fi
+before="# >>> Added by nlf - Northern Lights Forecast >>>"
+after="# <<< Added by nlf - Northern Lights Forecast <<<"
+line="0 0-8,18-23 * 9-12,1-3 * export DISPLAY=:0 && cd $PWD && $exe src/northern_lights_forecast/__main__.py >> t.txt 2>&1"
 if $has_p_option; then
     echo "$line"
 else
+    (crontab -l; echo "$before" ) | crontab -
+    (crontab -l; echo "$newpath" ) | crontab -
     (crontab -l; echo "$line" ) | crontab -
+    (crontab -l; echo "$after" ) | crontab -
     # (crontab -u "$username" -l; echo "$line" ) | crontab -u "$username" -
 fi
