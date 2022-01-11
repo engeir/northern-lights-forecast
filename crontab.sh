d3f34a05cbc6d1744d02b4b94fd62a87caee0a53
engeir <eirroleng@gmail.com>
2022-01-11 11:51:13 +0100

    :lipstic: Format shell script

--- crontab.sh
+++ crontab.sh
@@ -1,21 +1,25 @@
 #!/bin/sh
+
 has_p_option=false
 while getopts :p opt; do
     case $opt in
-        p) has_p_option=true ;;
-        :) echo "Missing argument for option -$OPTARG"; exit 1;;
-       \?) echo "Unknown option -$OPTARG"; exit 1;;
+    p) has_p_option=true ;;
+    :)
+        echo "Missing argument for option -$OPTARG"
+        exit 1
+        ;;
+    \?)
+        echo "Unknown option -$OPTARG"
+        exit 1
+        ;;
     esac
 done
 
 # here's the key part: remove the parsed options from the positional params
-shift $(( OPTIND - 1 ))
+shift $((OPTIND - 1))
 
-# username=$(whoami)
-PWD=$(pwd)
 exe=$(python -c "import sys;print(sys.executable)")
-if ! command -v tesseract > /dev/null 2>&1
-then
+if ! command -v tesseract >/dev/null 2>&1; then
     newpath="# Tesseract was not found. Make sure it is installed and is in PATH."
 else
     tesseract=$(dirname "$(which tesseract)")
@@ -24,15 +28,26 @@ fi
 before="# >>> Added by nlf - Northern Lights Forecast >>>"
 after="# <<< Added by nlf - Northern Lights Forecast <<<"
 line="0 0-8,18-23 * 9-12,1-3 * export DISPLAY=:0 && cd $PWD && $exe src/northern_lights_forecast/__main__.py > t.txt 2>&1"
-if $has_p_option; then
+if "$has_p_option"; then
     echo "$before"
     echo "$newpath"
     echo "$line"
     echo "$after"
 else
-    (crontab -l; echo "$before" ) | crontab -
-    (crontab -l; echo "$newpath" ) | crontab -
-    (crontab -l; echo "$line" ) | crontab -
-    (crontab -l; echo "$after" ) | crontab -
-    # (crontab -u "$username" -l; echo "$line" ) | crontab -u "$username" -
+    (
+        crontab -l
+        echo "$before"
+    ) | crontab -
+    (
+        crontab -l
+        echo "$newpath"
+    ) | crontab -
+    (
+        crontab -l
+        echo "$line"
+    ) | crontab -
+    (
+        crontab -l
+        echo "$after"
+    ) | crontab -
 fi
